6ba51ccd5d7d6318f39febcb8c325ee0e73f8b9a
darxsys <darxsys@gmail.com>
2014-10-15 15:13:56 +0200

    Added full support for automata usage in main.c

--- src/main.c
+++ src/main.c
@@ -295,7 +295,7 @@ int main(int argc, char* argv[]) {
             // automata candidates
             if (useAutomata) {
                 indices = filteredDatabaseIndicesAutomatonCreate(database,
-                    databaseStart, databaseLen, automata, automataLen, 
+                    databaseStart, databaseLen, automata,queriesLen, 
                     seedLen, scorer);
             }
 

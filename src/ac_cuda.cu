94787f6743e727791891d92738362eaaf6adf9b1
darxsys <darxsys@gmail.com>
2014-11-06 15:26:38 +0100

    Cuda copying is now functional, except DB :(.

--- src/ac_cuda.cu
+++ src/ac_cuda.cu
@@ -26,10 +26,10 @@ extern void* indicesTableCreateGpu(Chain** database,
 typedef vector<vector<int> > Candidates;
 typedef vector<int> Candidate;
 
-static TableGpu* copyTableToGpu(TabNode* table);
-static void deleteTableGpu(TableGpu* table);
+static TableGpu* copyTableToGpu(TabNode* table, TableGpu** hostStore);
+static void deleteTableGpu(TableGpu* table, TableGpu* hostStore);
 
-__global__ static void findCandidates(TableGpu* automata, 
+__global__ static void findCandidates(TableGpu** automata, 
     int automataLen, int* candidates);
 
 // ***************************************************************************
@@ -40,18 +40,23 @@ extern void* indicesTableCreateGpu(Chain** database,
 
     vector<TabNode*>* aut = static_cast<vector<TabNode*>*>(automata);
     vector<TableGpu*> gpuTables;
+    vector<TableGpu*> hostTables;
     gpuTables.reserve(automataLen);
+    hostTables.reserve(automataLen);
 
     for (int i = 0; i < automataLen; ++i) {
         TabNode* autH = (*aut)[i];
-        gpuTables.push_back(copyTableToGpu(autH));
+        TableGpu* hostTable;
+        gpuTables.push_back(copyTableToGpu(autH, &hostTable));
+        hostTables.push_back(hostTable);
 
+        printf("%p\n", gpuTables[i]);
     }
 
     dim3 dimGrid(1,1,1);
     dim3 dimBlock(1,1,1);
 
-    TableGpu* gpuTablesD;
+    TableGpu** gpuTablesD;
     CUDA_SAFE_CALL(cudaMalloc(&gpuTablesD, sizeof(TableGpu*) * automataLen));
     CUDA_SAFE_CALL(cudaMemcpy(gpuTablesD, &gpuTables[0], 
         sizeof(TableGpu*) * automataLen, 
@@ -62,10 +67,12 @@ extern void* indicesTableCreateGpu(Chain** database,
     findCandidates<<<dimGrid, dimBlock>>>(gpuTablesD, automataLen, candidatesD);
 
     // clean up
-    CUDA_SAFE_CALL(cudaFree(gpuTablesD));
     for (int i = 0; i < automataLen; ++i) {
-        deleteTableGpu(gpuTables[i]);
+        deleteTableGpu(gpuTables[i], hostTables[i]);
     }
+    CUDA_SAFE_CALL(cudaFree(gpuTablesD));
+    CUDA_SAFE_CALL(cudaFree(candidatesD));
+
     gpuTables.clear();
     return NULL;
 }
@@ -74,19 +81,19 @@ extern void* indicesTableCreateGpu(Chain** database,
 // ***************************************************************************
 // PRIVATE
 
-static TableGpu* copyTableToGpu(TabNode* table) {
-    TableGpu* copyAut = (TableGpu*) malloc(sizeof(TableGpu));
+static TableGpu* copyTableToGpu(TabNode* table, TableGpu** hostStore) {
+    *hostStore = (TableGpu*) malloc(sizeof(TableGpu));
 
-    copyAut->numStates = table->numStates;
-    copyAut->table = table->table;
+    (*hostStore)->numStates = table->numStates;
+    (*hostStore)->table = table->table;
 
     // flatten and copy positions vector
     int start = 0;
     vector<int> positions;
     vector<vector<uint16> > &v = table->positions;
 
-    for (int i = 0; i < copyAut->numStates; ++i) {
-        copyAut->table[i * TABLE_WIDTH + POSITIONS_START] = start;
+    for (int i = 0; i < (*hostStore)->numStates; ++i) {
+        (*hostStore)->table[i * TABLE_WIDTH + POSITIONS_START] = start;
 
         positions.insert(positions.end(), v[i].begin(), v[i].end());
 
@@ -100,32 +107,34 @@ static TableGpu* copyTableToGpu(TabNode* table) {
         sizeof(uint16) * positions.size(),
         cudaMemcpyHostToDevice));
 
-    copyAut->positions = positionsD;
+    (*hostStore)->positions = positionsD;
 
     // state table
     int* statesD;
-    CUDA_SAFE_CALL(cudaMalloc(&statesD, sizeof(int) * copyAut->numStates * TABLE_WIDTH));
+    CUDA_SAFE_CALL(cudaMalloc(&statesD, sizeof(int) * (*hostStore)->numStates * TABLE_WIDTH));
 
-    CUDA_SAFE_CALL(cudaMemcpy(statesD, copyAut->table,
-        sizeof(int) * copyAut->numStates * TABLE_WIDTH, 
+    CUDA_SAFE_CALL(cudaMemcpy(statesD, (*hostStore)->table,
+        sizeof(int) * (*hostStore)->numStates * TABLE_WIDTH, 
         cudaMemcpyHostToDevice));
 
-    copyAut->table = statesD;
+    (*hostStore)->table = statesD;
 
     TableGpu* autD;
     CUDA_SAFE_CALL(cudaMalloc(&autD, sizeof(TableGpu)));
 
-    CUDA_SAFE_CALL(cudaMemcpy(autD, copyAut, sizeof(TableGpu), 
+    CUDA_SAFE_CALL(cudaMemcpy(autD, (*hostStore), sizeof(TableGpu), 
         cudaMemcpyHostToDevice));
 
-    delete copyAut;
+    printf("autd %p\n", autD);
     return autD;
 }
 
-static void deleteTableGpu(TableGpu* table) {
-    CUDA_SAFE_CALL(cudaFree(table->table));
-    CUDA_SAFE_CALL(cudaFree(table->positions));
+static void deleteTableGpu(TableGpu* table, TableGpu* hostStore) {
+    CUDA_SAFE_CALL(cudaFree(hostStore->table));
+    CUDA_SAFE_CALL(cudaFree(hostStore->positions));
     CUDA_SAFE_CALL(cudaFree(table));    
+
+    free(hostStore);
 }
 
 // ***************************************************************************
@@ -133,7 +142,7 @@ static void deleteTableGpu(TableGpu* table) {
 // ***************************************************************************
 // GPU Modules
 
-__global__ static void findCandidates(TableGpu* automata, 
+__global__ static void findCandidates(TableGpu** automata, 
     int automataLen, int* candidates) {
 
     printf("heklo worls\n");

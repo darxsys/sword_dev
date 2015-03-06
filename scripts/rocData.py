65d897123086b853c220fb476515fe31ba1443ef
rvaser <robert.vaser@fer.hr>
2015-03-07 00:47:43 +0100

    Added scripts for evaluation

---
+++ scripts/rocData.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+
+import os, sys, getopt
+
+def main():
+
+    options = "i:j:f:n:h"
+    longOptions = ["help", "input=", "database=", "false-positive-rate=", "name="]
+
+    inputPath = None
+    databasePath = None
+    fpRate = 10000
+    name = "program"
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], options, longOptions)
+    except getopt.GetoptError as err:
+        print(str(err))
+        help()
+        sys.exit()
+
+    for o, a in opts:
+        if o in ("-i", "--input"):
+            inputPath = a
+        elif o in ("-j", "--database"):
+            databasePath = a
+        elif o in ("-f", "false-positive-rate"):
+            fpRate = int(a)
+        elif o in ("-n", "name"):
+            name = a
+        elif o in ("-h", "-help"):
+            help()
+            sys.exit()
+
+    if inputPath is None:
+        error("missing option: -i <file>")
+
+    if not os.path.isfile(inputPath):
+        error("non-existent file: -i {}".format(inputPath))
+
+    if databasePath is None:
+        error("missing option: -j <file>")
+
+    if not os.path.isfile(databasePath):
+        error("non-existent file: -j {}".format(databasePath))
+
+    if fpRate < 0:
+        error("invalid fp rate")
+
+    printRocData(processInput(inputPath, processDatabase(databasePath)), fpRate, name)
+    
+#******************************************************************************
+#******************************************************************************
+
+def processDatabase(databasePath):
+    database = open(databasePath).read().split('>')
+
+    db = {}
+
+    for seq in database:
+        if not seq:
+            continue
+
+        lines = seq.split("\n")
+        seqName = lines[0]
+        superFamily = ".".join(seqName.split(" ", 2)[1].split(".")[0:-1])
+
+        db[seqName.split(" ", 1)[0]] = superFamily
+
+    return db
+
+def processInput(inputPath, superFamilies):
+    input_ = open(inputPath).read().split("\n")
+
+    alignments = []
+
+    for i in range(len(input_)):
+        if not input_[i] or input_[i][0] == '#':
+            continue
+
+        if "[WARNING:" in input_[i] or "Using:" in input_[i] or "Query id" in input_[i]:
+            continue;
+
+        queryName = input_[i].split('\t', 1)[0]
+        targetName = input_[i].split('\t', 2)[1]
+
+        evalue = float(input_[i].split('\t')[-2:-1][0])
+
+        alignments.append([evalue, superFamilies[queryName] == superFamilies[targetName]])
+
+    return alignments
+
+def printRocData(alignments, maxFpRate, name):
+    alignments.sort(key=lambda x: x[0])
+
+    fpRate = 0
+    tpRate = 0
+
+    for align in alignments:
+        if fpRate == maxFpRate:
+            break
+
+        if align[1] == True:
+            tpRate += 1
+        else:
+            fpRate += 1
+
+        print name + ", " + str(tpRate) + ", " + str(fpRate)
+
+
+#******************************************************************************
+#******************************************************************************
+
+def error(message):
+    print("[ERROR] {}".format(message))
+    sys.exit()
+
+def help():
+    print(
+    "usage: python rocData.py [arguments ...]\n"
+    "arguments:\n"
+    "    -i, --input <file>\n"
+    "        (required)\n"
+    "        input file in bm8 format (blast m8 tabular output format)\n"
+    "    -j, --database <file>\n"
+    "        (required)\n"
+    "        input database file in fasta format\n"
+    "    -f, --false-positive-rate <int>\n"
+    "        default: 10000\n"
+    "        false positive rate\n"
+    "    -n, --name <string>\n"
+    "        default: program\n"
+    "        name of executable that produced the input file"
+    "    -h, --help\n"
+    "        prints out the help")
+
+#******************************************************************************
+#******************************************************************************
+
+main()
---
+++ scripts/simData.py
@@ -0,0 +1,213 @@
+#!/usr/bin/python
+
+import os, sys, getopt
+
+def main():
+
+    options = "i:j:h"
+    longOptions = ["help", "first=", "second="]
+
+    firstPath = None
+    secondPath = None
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], options, longOptions)
+    except getopt.GetoptError as err:
+        print(str(err))
+        help()
+        sys.exit()
+
+    for o, a in opts:
+        if o in ("-i", "--first"):
+            firstPath = a
+        elif o in ("-j", "--second"):
+            secondPath = a
+        elif o in ("-h", "-help"):
+            help()
+            sys.exit()
+
+    if firstPath is None:
+        error("missing option: -i <file>")
+
+    if not os.path.isfile(firstPath):
+        error("non-existent file: -i {}".format(firstPath))
+
+    if secondPath is None:
+        error("missing option: -j <file>")
+
+    if not os.path.isfile(secondPath):
+        error("non-existent file: -j {}".format(secondPath))
+
+    alignments1 = processInput(firstPath)
+    alignments2 = processInput(secondPath)
+
+    print firstPath + " vs " + secondPath
+
+    queryLen = len(alignments1) if len(alignments1) > len(alignments2) else len(alignments2)
+    print "QueryLen = " + str(queryLen)
+
+    printEvalueData(alignments1, alignments2)
+    printRankData(alignments1, alignments2)
+    printAlignLenData(alignments1, alignments2)
+    
+#******************************************************************************
+#******************************************************************************
+
+def processInput(inputPath):
+    input_ = open(inputPath).read().split("\n")
+
+    alignments = {}
+    rank = 1
+
+    sameAligments = []
+
+    for i in range(len(input_)):
+        if not input_[i] or input_[i][0] == '#':
+            continue
+
+        if "[WARNING:" in input_[i] or "Using:" in input_[i] or "Query id" in input_[i]:
+            continue;
+
+        queryName = input_[i].split('\t', 1)[0]
+
+        if not queryName in alignments:
+            alignments[queryName] = []
+            sameAligments = []
+            rank = 1
+
+        targetName = input_[i].split('\t', 2)[1]
+
+        if targetName in sameAligments:
+            continue
+
+        sameAligments.append(targetName)
+
+        alignLen = int(input_[i].split("\t", 4)[3])
+        evalue = float(input_[i].split('\t')[-2:-1][0])
+
+        alignments[queryName].append([targetName, alignLen, evalue, rank])
+
+        rank += 1
+
+    return alignments
+
+def printEvalueData(alignments1, alignments2):
+    evalues = [1.0e-250, 1.0e-100, 1.0e-75, 1.0e-50, 1.0e-25, 1.0e-10, 1.0e-7, 1.0e-4, 1.0e-2, 1, 10]
+
+    print "Evalue, Equal, %"
+
+    for evalue in evalues:
+        equal = 0
+        totalLen = 0
+
+        for queryName in alignments1:
+            temp1 = []
+            temp2 = []
+
+            for align in alignments1[queryName]:
+                if align[2] > evalue:
+                    break
+                temp1.append(align[0])
+
+            if queryName in alignments2:
+                for align in alignments2[queryName]:
+                    if align[2] > evalue:
+                        break
+                    temp2.append(align[0])
+
+            for t in temp1:
+                if t in temp2:
+                    equal += 1
+
+            totalLen += len(temp1)
+
+        total = 0 if totalLen == 0 else float(equal) / float(totalLen)
+        print str(evalue) + ", " + str(equal) + ", " + str(total)
+
+    print ""
+
+def printRankData(alignments1, alignments2):
+    ranks = [10, 25, 50, 75, 100, 150, 200, 300, 400, 500]
+
+    print "Rank, Equal, %"
+
+    for rank in ranks:
+        equal = 0
+        totalLen = 0
+
+        for queryName in alignments1:
+            temp1 = []
+            temp2 = []
+
+            for align in alignments1[queryName]:
+                if align[3] > rank:
+                    break
+                temp1.append(align[0])
+
+            if queryName in alignments2:
+                for align in alignments2[queryName]:
+                    if align[3] > rank:
+                        break
+                    temp2.append(align[0])
+
+            for t in temp1:
+                if t in temp2:
+                    equal += 1
+
+            totalLen += len(temp1)
+
+        total = 0 if totalLen == 0 else float(equal) / float(totalLen)
+        print str(rank) + ", " + str(equal) + ", " + str(total)
+
+    print ""
+
+def printAlignLenData(alignments1, alignments2):
+    print "AlignLen Equal, %"
+
+    equal = 0
+    totalLen = 0
+
+    for queryName in alignments1:
+        temp1 = {}
+        temp2 = {}
+
+        for align in alignments1[queryName]:
+            temp1[align[0]] = align[1]
+
+        if queryName in alignments2:
+            for align in alignments2[queryName]:
+                temp2[align[0]] = align[1]            
+
+        for key in temp2:
+            if key in temp1:
+                if abs(temp1[key] - temp2[key]) < 5:
+                    equal += 1
+                totalLen += 1
+
+    total = 0 if totalLen == 0 else float(equal) / float(totalLen)
+    print str(equal) + ", " + str(total)
+
+#******************************************************************************
+#******************************************************************************
+
+def error(message):
+    print("[ERROR] {}".format(message))
+    sys.exit()
+
+def help():
+    print(
+    "usage: python simData.py [arguments ...]\n"
+    "arguments:\n"
+    "    -i, --first <file>\n"
+    "        (required)\n"
+    "        first input file in bm8 format (blast m8 tabular output format)\n"
+    "    -j, --second <file>\n"
+    "        (required)\n"
+    "        second input file in bm8 format (blast m8 tabular output format)\n"
+    "    -h, --help\n"
+    "        prints out the help")
+
+#******************************************************************************
+#******************************************************************************
+
+main()
